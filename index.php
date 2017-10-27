a4f383c8e793e2640ed03df9d7e317c30f960978
Bulat <sadikovba@gmail.com>
2017-10-27 18:17:45 +0300

    Initial commit

---
+++ index.php
@@ -0,0 +1,68 @@
+﻿<?php include('header.php'); ?>
+
+<section>
+	<div class="container">
+		<h1>Hello from the other side</h1>
+		<h2>Hello from the other side</h2>
+		<h3>Hello from the other side</h3>
+		<p>Экстренная помощь MIG</p>
+		<ul>
+			<li>Экстренная помощь MIG</li>
+			<li>Экстренная помощь MIG</li>
+			<li>Экстренная помощь MIG</li>
+		</ul>
+
+		<a href="#" class="fancy btn" data-src="#modal-order-1">Открыть order</a>
+		<a href="#" class="fancy btn" data-src="#modal-order-2">Открыть order 2</a>
+		<a href="img/img.png" class="fancy btn">Открыть img</a>
+		<a href="https://www.youtube.com/watch?v=dQw4w9WgXcQ" class="fancy-video btn">Открыть youtube</a>
+		<a href="https://yandex.ru/map-widget/v1/-/CBQneLhsOA" class="fancy-map btn" data-type="iframe">схема проезда</a>
+
+		<div class="scroll">
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент.</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+			<p>Джордан - так зовут рабочего - воспользовался передвижным туалетом на стройплощадке в Сиднее, как раз там, где пять месяцев назад с ним произошел подобный инцидент</p>
+		</div>
+
+		<div class="row">
+			<div class="grid-3">3333</div>
+			<div class="grid-5">5555</div>
+			<div class="grid-4">4444</div>
+			<div class="grid-4 grid-off-3">4444</div>
+		</div>
+		<div class="row-ng">
+			<div class="grid-3">3333</div>
+			<div class="grid-5">5555</div>
+			<div class="grid-4">4444</div>
+			<div class="grid-4 grid-off-3">4444</div>
+		</div>
+
+	</div>
+</section>
+
+<section class="review-sec">
+	<div class="container">
+		<div class="review row">
+			<div class="grid-12">
+				<div class="review__item">
+					<div class="review__item-img">
+						<img src="" alt="">
+					</div>
+					<div class="review__item-info">
+						<div class="review__item-title"></div>
+						<div class="review__item-text"></div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</section>
+
+
+<?php include('footer.php'); ?>
